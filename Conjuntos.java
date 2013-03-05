1bea16832265496b56621b6fb9a79cf0239ddb36
ViniciusMatheus <juvenal2014@gmail.com>
2013-03-04 23:14:14 -0300

    Update Conjuntos.java

--- Conjuntos.java
+++ Conjuntos.java
@@ -1,41 +1,37 @@
 package ...;
 
-public class Main 
-{
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) 
-    { 
-        Conjunto<Integer> teste1 = new Conjunto<>();
-        Conjunto<String> teste2 = new Conjunto<>();
-        Conjunto<Integer> teste3 = new Conjunto<>();
- 
-        teste1.setElemento(1);
-        teste1.setElemento(2);
-        teste1.setElemento(3);
-        
-        teste3.setElemento(4);
-        
-        teste2.setElemento("teste2");
-        teste2.setElemento("teste3");
-        
-        teste1.getElemento();
-        
-        teste2.getElemento();
-        
-        
-        
-        System.out.println("\n--------------------\n");
-        
-        
-        Conjunto<Integer> A = new Conjunto<>();
-        
-        A.setElemento(1);
-        A.setElemento(3);
-        A.setElemento(5);
-        A.setElemento(7);
-        
-        A.getElemento();
-        
-        A.pertinencia(3, A);
+public class Conjunto<Tipo> 
+{
+    private List<Tipo> conjunto;
+    
+    public Conjunto()
+    {
+        conjunto = new ArrayList<>();
+    }
+    
+    public void setElemento(Tipo elemento)
+    {
+        conjunto.add(elemento);
+    }
+    
+    public void getElemento()
+    {
+        System.out.println(conjunto.toString());
+    }
+    
+    public void pertinencia(Tipo valor, Conjunto conjunto)
+    {
+        if(this.conjunto.contains(valor))
+        {
+            System.out.println("Elemento " + valor + " pertence ao conjunto " + conjunto);
+        }
+        else
+        {
+            System.out.println("Elemento " + valor + " não pertence ao conjunto " + conjunto);
+        }
     }
 }
+
