ef16d6ce40b211361c2e7a7233bb345c28d370aa
ViniciusMatheus <juvenal2014@gmail.com>
2013-06-28 12:58:50 -0300

    Update Conjuntos.java

--- Conjuntos.java
+++ Conjuntos.java
@@ -1,5 +1,5 @@
 
-package [sua package];
+package [sua-package];
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,6 +7,7 @@ import java.util.List;
 public class Conjunto<Tipo extends Comparable> 
 {
     private List<Tipo> conjunto;
+//    int [] item;
     
     public Conjunto()
     {
@@ -47,8 +48,8 @@ public class Conjunto<Tipo extends Comparable>
     
     
 //    operações com comjuntos
-//---------------------------------------------------------------
-//---------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
     
     public boolean igualdade(Conjunto b) //100%
     {
@@ -133,16 +134,15 @@ public class Conjunto<Tipo extends Comparable>
     public String diferenca(Conjunto b) // 100%
     {
         Conjunto c = new Conjunto();
-                
+        
         for (int i = 0; i < conjunto.size(); i++)
         {
-                if(!conjunto.contains(b.conjunto.get(i)))
-                {
-                    c.adicionar(conjunto.get(i));
-                    c.adicionar(b.conjunto.get(i).toString());
-                }
+            if(!conjunto.contains(b.conjunto.get(i)))
+            {
+                c.adicionar(conjunto.get(i));
+                c.adicionar((Comparable) b.conjunto.get(i));
+            }
         }
-        
         return c.getConjunto();
     }
     
@@ -158,7 +158,97 @@ public class Conjunto<Tipo extends Comparable>
                
             }
         }
-        
         return c.getConjunto();
+    }
+    
+    // radix sort
+    
+    public void radixSort(int[] a, int tamanho)  // 100%
+    {
+        int[] b = new int[a.length];
+        int[] bOrig = b;
+
+        int rshift = 0;
+        for (int mascara = ~(-1 << tamanho); mascara != 0; mascara <<= tamanho, rshift += tamanho) 
+        {
+            int[] contador = new int[1 << tamanho];
+            
+            for (int p = 0; p < a.length; ++p) {
+                int chave = (a[p] & mascara) >> rshift;
+                ++contador[chave];
+            }
+
+            for (int i = 1; i < contador.length; ++i) 
+            {
+                contador[i] += contador[i - 1];
+            }
+
+            for (int p = a.length - 1; p >= 0; --p) 
+            {
+                int chave = (a[p] & mascara) >> rshift;
+                --contador[chave];
+                b[contador[chave]] = a[p];
+            }
+
+            int[] temp = b;
+            b = a;
+            a = temp;
+        }
+
+        if (a == bOrig) 
+        {
+            System.arraycopy(a, 0, b, 0, a.length);
+        }
+    }
+    // =========================================================================
+    // endorelaçõoes
+    // =========================================================================
+    
+    //reflexividade
+    public void reflexiva(int[][] matriz) 
+    {
+        for (int i = 0; i < matriz.length; i++) 
+        {
+            for (int e = 0; e < matriz.length; e++) 
+            {
+                if (matriz[i][e] == 0) 
+                {
+                    System.out.println("reflexiva: " + matriz[i][e]);
+                }
+            }
+        }
+        System.out.println("não reflexiva");
+    }
+
+    //transitividade
+    public void transitiva(int[][] matriz) 
+    {
+        for (int i = 0; i >= matriz.length; i++) 
+        {
+            for (int e = 0; e >= matriz.length; e++) 
+            {
+                if (matriz[i][e] == 1) 
+                {
+                    System.out.println("transitiva: " + matriz[i][e]);
+                }
+            }
+        }
+        System.out.println("não transitiva");
+    }
+
+    //simetria
+    public void simetrica(int[][] matriz) 
+    {
+        for (int i = 0; i >= matriz.length; i++) 
+        {
+            for (int e = 0; e >= matriz.length; e++) 
+            {
+                if (matriz[i][e] == matriz[e][i]) 
+                {
+                    System.out.println("simetrica: " + matriz[i][e]);
+                }
+            }
+        }
+        System.out.println("não simetrica");
     }
 }
